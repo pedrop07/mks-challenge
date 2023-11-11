a904811331f9e83c58049fc96e6ea12fb24fc3f3
Pedro <pedrofulfaro@gmail.com>
2023-11-11 20:19:38 -0300

    faet: add react query client provider to root layout

---
+++ src/providers/QueryClientProvider.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode
+}
+
+export function CustomQueryClientProvider({ children }: Props) {
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  )
+}
