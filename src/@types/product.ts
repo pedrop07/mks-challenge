fb8ae76a26dd0d40c6202d0ab57468cbddf7c622
Pedro <pedrofulfaro@gmail.com>
2023-11-12 13:15:38 -0300

    feat: add Product interface

---
+++ src/@types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  photo: string;
+  price: string;
+  amount: number;
+  createdAt: string;
+  updatedAt: string;
+}
