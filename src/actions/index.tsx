e91b4674040a9e32a5ace86141a249fda1e56307
Pedro <pedrofulfaro@gmail.com>
2023-11-12 16:43:05 -0300

    feat: add getProducts server action

---
+++ src/actions/index.tsx
@@ -0,0 +1,41 @@
+'use server';
+
+import { Product } from '@/@types/Product';
+import { api } from '@/services/api';
+
+interface GetProductsResponse {
+  products: Product[];
+  count: number;
+}
+
+const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function getProducts(): Promise<GetProductsResponse> {
+  function generateRandomInteger() {
+    const randomDecimal = Math.random();
+
+    const randomInteger = Math.floor(randomDecimal * 9) + 2;
+
+    return randomInteger;
+  }
+
+  if (!apiURL) {
+    console.error(
+      'A variável NEXT_PUBLIC_API_URL não está definida. Certifique-se de configurá-la corretamente.',
+    );
+    throw new Error('Internal server error');
+  }
+
+  const { data } = await api.get<GetProductsResponse>('/products');
+
+  if (data) {
+    const products = data.products.map((product) => ({
+      ...product,
+      amount: generateRandomInteger(),
+    }));
+
+    return { ...data, products };
+  }
+
+  throw new Error('Internal server error');
+}
